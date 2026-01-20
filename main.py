0bf3faf491d8d89dd5901567e1c53391e3da6b77
Elenaa78 <elenajaworska13@gmail.com>
2026-01-20 20:12:32 +0100

    Implementacja pełnego CRUD (Create, Read, Update, Delete) dla wizyt

--- main.py
+++ main.py
@@ -32,6 +32,38 @@ def dodaj_wizyte(wizyta: schemas.WizytaCreate, db: Session = Depends(get_db)):
 
     return db_wizyta
 
+@app.delete("/api/wizyty")
+def usun_wizyte(id_wizyty: int, db: Session = Depends(get_db)):
+    wizyta_do_usuniecia = db.query(models.WizytaDB).filter(models.WizytaDB.id == id_wizyty).first()
+
+    if wizyta_do_usuniecia:
+        db.delete(wizyta_do_usuniecia)
+        db.commit()
+        return {"message": f"Wizyta o id {id_wizyty} została usunięta"}
+    
+    return {"message": "Nie znaleziono takiej wizyty"}
+
+@app.put("/api/wizyty", response_model=schemas.Wizyta)
+def edytuj_wizyte(id_wizyty: int, nowe_dane: schemas.WizytaCreate, db: Session = Depends(get_db)):
+    wizyta = db.query(models.WizytaDB).filter(models.WizytaDB.id == id_wizyty).first()
+
+    if wizyta:
+        wizyta.klient = nowe_dane.klient
+        wizyta.data = nowe_dane.data
+        wizyta.godzina = nowe_dane.godzina
+        wizyta.opis = nowe_dane.opis
+
+        db.commit()
+        db.refresh(wizyta)
+
+        return wizyta
+
+    return None
+
+@app.get("/api/wizyty/{wizyta_id}", response_model=schemas.Wizyta)
+def pobierz_wizyte(id_wizyty: int, db: Session = Depends(get_db)):
+    return db.query(models.WizytaDB).filter(models.WizytaDB.id == id_wizyty).first()
+
 @app.get("/api/wizyty", response_model=list[schemas.Wizyta])
 def pobierz_wizyty(db: Session = Depends(get_db)):
     return db.query(models.WizytaDB).all()
