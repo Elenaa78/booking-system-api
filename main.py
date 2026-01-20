04f236316e0d675b7c14198b0c5c868d15e7cd65
Elenaa78 <elenajaworska13@gmail.com>
2026-01-20 21:18:49 +0100

    Dodano obsługę błędów 404 dla nieistniejącyh wizyt.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import uvicorn
 
@@ -32,40 +32,48 @@ def dodaj_wizyte(wizyta: schemas.WizytaCreate, db: Session = Depends(get_db)):
 
     return db_wizyta
 
-@app.delete("/api/wizyty")
+@app.delete("/api/wizyty/{id_wizyty}")
 def usun_wizyte(id_wizyty: int, db: Session = Depends(get_db)):
     wizyta_do_usuniecia = db.query(models.WizytaDB).filter(models.WizytaDB.id == id_wizyty).first()
 
-    if wizyta_do_usuniecia:
-        db.delete(wizyta_do_usuniecia)
-        db.commit()
-        return {"message": f"Wizyta o id {id_wizyty} została usunięta"}
+    if not wizyta_do_usuniecia:
+        raise HTTPException(404, "Taka wizyta nie istnieje")
     
-    return {"message": "Nie znaleziono takiej wizyty"}
+    db.delete(wizyta_do_usuniecia)
+    db.commit()
 
-@app.put("/api/wizyty", response_model=schemas.Wizyta)
+    return {"message": f"Wizyta o id {id_wizyty} została usunięta"}
+
+
+@app.put("/api/wizyty/{id_wizyty}", response_model=schemas.Wizyta)
 def edytuj_wizyte(id_wizyty: int, nowe_dane: schemas.WizytaCreate, db: Session = Depends(get_db)):
     wizyta = db.query(models.WizytaDB).filter(models.WizytaDB.id == id_wizyty).first()
 
-    if wizyta:
-        wizyta.klient = nowe_dane.klient
-        wizyta.data = nowe_dane.data
-        wizyta.godzina = nowe_dane.godzina
-        wizyta.opis = nowe_dane.opis
+    if not wizyta:
+        raise HTTPException(404, "Nie znaleziono wizyty")
 
-        db.commit()
-        db.refresh(wizyta)
+    wizyta.klient = nowe_dane.klient
+    wizyta.data = nowe_dane.data
+    wizyta.godzina = nowe_dane.godzina
+    wizyta.opis = nowe_dane.opis
 
-        return wizyta
+    db.commit()
+    db.refresh(wizyta)
 
-    return None
+    return wizyta
 
-@app.get("/api/wizyty/{wizyta_id}", response_model=schemas.Wizyta)
+@app.get("/api/wizyty/{id_wizyty}", response_model=schemas.Wizyta)
 def pobierz_wizyte(id_wizyty: int, db: Session = Depends(get_db)):
-    return db.query(models.WizytaDB).filter(models.WizytaDB.id == id_wizyty).first()
+    wynik = db.query(models.WizytaDB).filter(models.WizytaDB.id == id_wizyty).first()
+
+    if not wynik:
+        raise HTTPException(404, "Wynik jest pusty")
+    
+    return wynik
 
 @app.get("/api/wizyty", response_model=list[schemas.Wizyta])
 def pobierz_wizyty(db: Session = Depends(get_db)):
+
     return db.query(models.WizytaDB).all()
 
 if __name__ == "__main__":
